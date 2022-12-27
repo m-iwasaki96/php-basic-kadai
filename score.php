cfe7e7391ac298fa308745cd36f254f76443a139
m-iwasaki96 <iwasaki.ocu96@gmail.com>
2022-12-28 00:48:33 +0900

    first commit

---
+++ score.php
@@ -0,0 +1,30 @@
+<?php
+$score1 = 80;
+$score2 = 60;
+$score3 = 55;
+$score4 = 40;
+$score5 = 100;
+$score6 = 25;
+$score7 = 80;
+$score8 = 95;
+$score9 = 30;
+$score10 = 60;
+
+$sum = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+$avg = $sum / 10;
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo $sum; ?></h1>
+    <h2><?php echo $avg;?></h2>
+    
+</body>
+</html>
